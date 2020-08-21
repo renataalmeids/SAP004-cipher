8dff0798b6f20ff21604be95dda3b35bebf25415
renataalmeids <ras_almeida@hotmail.com>
2020-08-21 12:00:14 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,28 @@
+
+const cipher = {
+  encode: function(offset,string) {
+        if (offset =="" || string==""||typeof offset!=="number" ||typeof string!=="string"){
+        throw new TypeError('Digite uma mensagem ou senha válida.')
+      } else{
+
+        let menssagemCipher = "";
+      for (let i=0; i<string.length; i++){
+        let codAsc = string.charCodeAt(i);
+        let forEncode = ((codAsc -65 + offset) % 26) + 65;
+        menssagemCipher += String.fromCharCode(forEncode); 
+      } return menssagemCipher;
+    } 
+  },
+
+  decode: function(offset,string){
+    let menssagemCipher = "";
+    for (let i=0; i<string.length; i++){
+      let codAsc = string.charCodeAt(i);
+      let forEncode = ((codAsc -90 - offset) % 26) + 90;
+      menssagemCipher += String.fromCharCode(forEncode); 
+      
+    } return menssagemCipher;
+  }
+}
+
+export default cipher;
---
+++ index.js
@@ -0,0 +1,17 @@
+import cipher from './cipher.js';
+
+document.getElementById("Encode").addEventListener("click",cifrar);
+function cifrar(event){
+    event.preventDefault();
+    let offset= Number(document.getElementById("position").value);
+    let string= document.getElementById("menssagesend").value.toUpperCase();
+    document.getElementById("answer").textContent= cipher.encode(offset,string);
+}
+
+document.getElementById("Decode").addEventListener("click",decifrar);
+function decifrar(event){
+    event.preventDefault();
+    let offset= Number(document.getElementById("position").value);
+    let string= document.getElementById("menssagesend").value.toUpperCase();
+    document.getElementById("answer").textContent=cipher.decode(offset,string);
+}
