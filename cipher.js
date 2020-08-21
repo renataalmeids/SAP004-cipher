a7e61a3eec4902287c3ef08603c259f54dc915eb
renataalmeids <ras_almeida@hotmail.com>
2020-08-21 12:12:56 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,26 +1,26 @@
 
 const cipher = {
-  encode: function(offset,string) {
-        if (offset =="" || string==""||typeof offset!=="number" ||typeof string!=="string"){
-        throw new TypeError('Digite uma mensagem ou senha válida.')
-      } else{
+  encode: function (offset, string) {
+    if (offset == "" || string == "" || typeof offset !== "number" || typeof string !== "string") {
+      throw new TypeError('Digite uma mensagem ou senha válida.')
+    } else {
 
-        let menssagemCipher = "";
-      for (let i=0; i<string.length; i++){
+      let menssagemCipher = "";
+      for (let i = 0; i < string.length; i++) {
         let codAsc = string.charCodeAt(i);
-        let forEncode = ((codAsc -65 + offset) % 26) + 65;
-        menssagemCipher += String.fromCharCode(forEncode); 
+        let forEncode = ((codAsc - 65 + offset) % 26) + 65;
+        menssagemCipher += String.fromCharCode(forEncode);
       } return menssagemCipher;
-    } 
+    }
   },
 
-  decode: function(offset,string){
+  decode: function (offset, string) {
     let menssagemCipher = "";
-    for (let i=0; i<string.length; i++){
+    for (let i = 0; i < string.length; i++) {
       let codAsc = string.charCodeAt(i);
-      let forEncode = ((codAsc -90 - offset) % 26) + 90;
-      menssagemCipher += String.fromCharCode(forEncode); 
-      
+      let forEncode = ((codAsc - 90 - offset) % 26) + 90;
+      menssagemCipher += String.fromCharCode(forEncode);
+
     } return menssagemCipher;
   }
 }
